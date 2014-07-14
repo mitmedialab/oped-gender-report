8c282a0c9669183618d97490f259a604c61a755d
pishkin <pamela.mishkin@gmail.com>
2014-07-14 15:09:15 -0400

    Fixing tabs/spaces issue

--- data_acquisition/base_meta.py
+++ data_acquisition/base_meta.py
@@ -10,15 +10,15 @@ class BaseMeta():
 	url = ""
 	soup = BeautifulSoup.BeautifulSoup("")
 
-        def __init__(self, url=None, soup=None):
-                if url:
-                        self.url = url
-                if soup:
-                        self.soup = soup
+	def __init__(self, url=None, soup=None):
+		if url:
+			self.url = url
+		if soup:
+                	self.soup = soup
 
 	def make_soup(self, stories_id=None, url=None, file_name=None):
-                self.url = ""
-                self.soup = BeautifulSoup.BeautifulSoup("")
+		self.url = ""
+		self.soup = BeautifulSoup.BeautifulSoup("")
 		if stories_id:
 			api_key = yaml.load(open('config.yaml'))['mediacloud']['api_key']
 			mc = mediacloud.api.MediaCloud(api_key)
@@ -27,22 +27,22 @@ class BaseMeta():
 			self.soup = BeautifulSoup.BeautifulSoup(story[u'raw_first_download_file'])
 		elif url:
 			self.url = url
-                        self.soup = BeautifulSoup.BeautifulSoup(urllib2.urlopen(url).read())
+			self.soup = BeautifulSoup.BeautifulSoup(urllib2.urlopen(url).read())
 		elif file_name:
 			self.url = file_name
 			self.soup = BeautifulSoup.BeautifulSoup(open(file_name,'r').read())
                         
-        byline_tags = []
+	byline_tags = []
 	def get_byline(self, byline_tags=None):
 		if byline_tags:
 			self.byline_tags = byline_tags
-                for t in self.byline_tags:
-                        if self.soup.find(t[0],{t[1]:t[2]}) is not None:
-                                if t[0]=='meta':
-                                        return self.soup.find(t[0], {t[1]:t[2]})['content']
-                                else:
-                                        return self.soup.find(t[0], {t[1]:t[2]}).text
-                return ''
+		for t in self.byline_tags:
+			if self.soup.find(t[0],{t[1]:t[2]}) is not None:
+				if t[0]=='meta':
+					return self.soup.find(t[0], {t[1]:t[2]})['content']
+				else:
+					return self.soup.find(t[0], {t[1]:t[2]}).text
+		return ''
 
 	url_section_patterns = []	
 	section_tags = []
@@ -55,84 +55,84 @@ class BaseMeta():
 			if re.match(reg,self.url):
 				return re.match(reg,self.url).group('section')
 		for tag in self.section_tags:
-                        if self.soup.find(tag[0],{tag[1]:tag[2]}) is not None:
-                                if tag[0]=='meta':
-                                        return self.soup.find(tag[0], {tag[1]:tag[2]})['content']
-                                else:
-                                        return self.soup.find(tag[0], {tag[1]:tag[2]}).text
-                return ''
+			if self.soup.find(tag[0],{tag[1]:tag[2]}) is not None:
+				if tag[0]=='meta':
+					return self.soup.find(tag[0], {tag[1]:tag[2]})['content']
+				else:
+					return self.soup.find(tag[0], {tag[1]:tag[2]}).text
+		return ''
 
-        opinion_sections = []
-        def is_opinion(self, opinion_sections=None):
-                if opinion_sections:
-                        self.opinion_sections = opinion_sections
-                return get_section().lower() in opinion_sections
+	opinion_sections = []
+	def is_opinion(self, opinion_sections=None):
+		if opinion_sections:
+			self.opinion_sections = opinion_sections
+		return get_section().lower() in opinion_sections
                        
 
 class NYTimesMeta(BaseMeta):
 
-        byline_tags = [['meta','name','author'],['span','class','byline-author'],['meta','name','clmst']]
+	byline_tags = [['meta','name','author'],['span','class','byline-author'],['meta','name','clmst']]
         
-        url_section_patterns = [r'http://www.nytimes.com/[0-9]{4}/[0-9]{2}/[0-9]{2}/(?P<section>[A-Za-z0-9/]+)/']
-        section_tags = [['meta','name',re.compile('cg',re.I)]]
+	url_section_patterns = [r'http://www.nytimes.com/[0-9]{4}/[0-9]{2}/[0-9]{2}/(?P<section>[A-Za-z0-9/]+)/']
+	section_tags = [['meta','name',re.compile('cg',re.I)]]
         
-        opinion_sections = ['opinion']
+	opinion_sections = ['opinion']
         
 
 class WashingtonPostMeta(BaseMeta):
 
-        byline_tags = [['span', 'class', 'blog-byline'], ['meta', 'name', 'dc.creator'],['meta','name','DC.creator'],['div','id','byline']]
+	byline_tags = [['span', 'class', 'blog-byline'], ['meta', 'name', 'dc.creator'],['meta','name','DC.creator'],['div','id','byline']]
                 
-        url_section_patterns = [r'http://feeds.washingtonpost.com/c/34656/f/[0-9]+/s/[A-Za-z0-9]+/(sc/[0-9]+/l|l)/0L0Swashingtonpost0N0C(?P<section>[A-Za-z0-9]+?)s0C[A-Za-z0-9]+/story01.htm',r'http://www.washingtonpost.com/wp-dyn/content/article/[0-9]{4}/[0-9]{2}/[0-9]{2}/AR[0-9]+.html?(nav|wprss)=rss_(?P<section>[A-Za-z0-9]+)(/columns$|/industries$|s$|$)']
-        section_tags = [['meta', 'name', 'section']]
+	url_section_patterns = [r'http://feeds.washingtonpost.com/c/34656/f/[0-9]+/s/[A-Za-z0-9]+/(sc/[0-9]+/l|l)/0L0Swashingtonpost0N0C(?P<section>[A-Za-z0-9]+?)s0C[A-Za-z0-9]+/story01.htm',r'http://www.washingtonpost.com/wp-dyn/content/article/[0-9]{4}/[0-9]{2}/[0-9]{2}/AR[0-9]+.html?(nav|wprss)=rss_(?P<section>[A-Za-z0-9]+)(/columns$|/industries$|s$|$)']
+	section_tags = [['meta', 'name', 'section']]
 
-        opinion_sections = ['opinion','blog']
+	opinion_sections = ['opinion','blog']
 
 
 class WSJMeta(BaseMeta):
     
-        byline_tags = [['meta', 'name', 'author']]
-    
-        section_tags = [['meta', 'name', 'article.section']]
+	byline_tags = [['meta', 'name', 'author']]
+	
+	section_tags = [['meta', 'name', 'article.section']]
 
 
 class LATimesMeta(BaseMeta):
 
-        byline_tags = [['span', 'class', 'byline'], ['div','class','trb-bylines'], ['address','class','trb_columnistInfo_columnistPortrait'], ['meta', 'name', 'author'],['div','id','mod-article-byline']]
+	byline_tags = [['span', 'class', 'byline'], ['div','class','trb-bylines'], ['address','class','trb_columnistInfo_columnistPortrait'], ['meta', 'name', 'author'],['div','id','mod-article-byline']]
     
-        url_section_patterns = [r'http://feeds.latimes.com/~r/(latimes/)*(?P<section>[A-Za-z/]+)/~3']
-        section_tags = [['meta', 'name', 'article.section']]
+	url_section_patterns = [r'http://feeds.latimes.com/~r/(latimes/)*(?P<section>[A-Za-z/]+)/~3']
+	section_tags = [['meta', 'name', 'article.section']]
 
-        opinion_sections = ['news/opinion','OpinionLa']
+	opinion_sections = ['news/opinion','OpinionLa']
 
 
 class HuffPoMeta(BaseMeta):
 
-        byline_tags = [['meta','name','author']]
-        def get_byline(self):
-                return BaseMeta.get_byline(self).split(' and ')
+	byline_tags = [['meta','name','author']]
+	def get_byline(self):
+		return BaseMeta.get_byline(self).split(' and ')
 
 
 class SalonMeta(BaseMeta):
         
-        byline_tags = [['span','class','byline'],['a','rel','author']]
+	byline_tags = [['span','class','byline'],['a','rel','author']]
 
 
 class DailyBeastMeta(BaseMeta):
 
-        byline_tags = [['meta','name','authors'],['a','class','more-by-author']]
+	byline_tags = [['meta','name','authors'],['a','class','more-by-author']]
 
 
 class YaleMeta(BaseMeta):
 
-        byline_tags = [['div','class','entry-authors']]
+	byline_tags = [['div','class','entry-authors']]
 
 
 class PrincetonMeta(BaseMeta):
 
-        byline_tags = [['p','class','byline']]
+	byline_tags = [['p','class','byline']]
 
 
 class ColumbiaMeta(BaseMeta):
 
-        byline_tags = [['div','class','article-authors']]
+	byline_tags = [['div','class','article-authors']]
