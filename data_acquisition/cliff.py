bb25e4f4fd5ba8dc5c0f3cdc92c7df5cf61d9ce5
Natematias <natematias@gmail.com>
2014-08-11 14:14:08 -0400

    added a small library to access CLIFF to do named entity detection on mediacloud articles

---
+++ data_acquisition/cliff.py
@@ -0,0 +1,14 @@
+import requests
+import json
+import yaml
+
+CLIFF_URL = yaml.load(open('config.yaml'))['cliff']['url']
+
+def people(content):
+  req = requests.post(CLIFF_URL,params={"q":content})
+  j = json.loads(req.text)
+  if(len(j[u'results'])>0):
+    return j[u'results'][u'people']
+  else:
+    print j.keys()
+  return []
